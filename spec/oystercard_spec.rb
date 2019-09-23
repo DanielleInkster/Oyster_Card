64b9963c6d5a6a14ecca4232a81b76e0697cd0ac
Jessie <lonsdale.jess@gmail.com>
2019-09-23 14:03:31 +0100

    ch 3 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,17 @@
+require "oystercard"
+
 describe Oystercard do
 
+  it "is an Oystercard object" do
+    expect(subject.class).to eq Oystercard
+  end
+
+  it "card starts with a balance of zero" do
+    expect(subject.balance).to eq 0
+  end
 
 
 
 
 
-
-
-
-  
 end
