b761c713d94938cf0ce51f318d990b3279b13dcb
DanielleInkster <danielle.inkster@gmail.com>
2019-09-27 16:22:24 +0100

    Created JourneyLog class

---
+++ lib/journey_log.rb
@@ -0,0 +1,8 @@
+require_relative 'oystercard'
+require_relative 'station'
+require_relative 'journey'
+
+class JourneyLog
+
+
+end
---
+++ spec/journey_log_spec.rb
@@ -0,0 +1,7 @@
+require './lib/journey_log.rb'
+describe JourneyLog do
+
+  it "is an JourneyLog object" do
+    expect(subject.class).to eq JourneyLog
+  end
+end
